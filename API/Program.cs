414b07cf6c5153ab6859f451efbe9d1462a67e2e
Milos <m.miljevic@hotmail.com>
2023-02-17 16:02:36 +0100

    adding angular

--- API/Program.cs
+++ API/Program.cs
@@ -9,14 +9,16 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<DataContex>(opt =>
 {
- opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
